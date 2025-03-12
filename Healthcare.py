dd592294fe47f084e33a1af95aa37b5b3ae53f6f
Akshata <akshata2890@gmail.com>
2025-03-12 16:40:10 +0530

    Delete Healthcare.py

--- Healthcare.py
+++
@@ -1,4 +0,0 @@
-## importing libraries
-
-import pandas as pd                           #pandas library is used for data manipulation and analysis
-import matplotlib.pyplot as plt                  #imported matplotlib and seaborn
