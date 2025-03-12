170cfd917141cb0195444c5c5ba29424f0a00940
Akshata <53058275+AkshataAhire@users.noreply.github.com>
2025-03-12 14:51:51 +0530

    py files added
    py files added as streamlit cannot run jupyter notebooks

---
+++ Healthcare.py
@@ -0,0 +1,4 @@
+## importing libraries
+
+import pandas as pd                           #pandas library is used for data manipulation and analysis
+import matplotlib.pyplot as plt                  #imported matplotlib and seaborn
